e15675708f71ecb0ae942f7133ddac4e54a8e21d
Daria <daria.cheliubeeva@mail.ru>
2025-02-27 12:54:18 +0300

    no folder

--- reacttodolist/src/App.test.js
+++ reacttodolist/src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('есть кнопка', () => {
+  render(<App />);
+
+  const buttonAdd = screen.getByText("Добавить дело");
+  expect(buttonAdd).toBeDefined();
+
+});
+
 test('есть заголовок', () => {
   render(<App />);
-  const linkElement = screen.getByText("Active");
-  expect(linkElement).toBeInTheDocument();
+
+  const title = screen.getByText("Список дел");
+  expect(title).toBeDefined();
 });
+
+test('есть инпут', () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText("Введите название дела");
+  expect(input).toBeDefined();
+});
+
--- reacttodolist/src/components/todoTitle/index.js
+++ reacttodolist/src/components/todoTitle/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import s from "./style.module.css"
 
 
-// type PropTitle = {
-//     title: string,
-// }
-// const TodoListTitle = ({ title }: PropTitle) => {
-const TodoListTitle = (title) => {
-    // return <h1 >{title}</h1>
+const TodoListTitle = ({ title }) => {
     return <h1 className={s.title}>{title}</h1>
 };
 
